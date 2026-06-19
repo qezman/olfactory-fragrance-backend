d7b855f3e2c1e9537c4057d37d06ab42cfeab3d5
Kazeem Jimoh <holaryinka5050@gmail.com>
2026-06-19 15:26:27 +0100

    fix: connection pool string

--- src/modules/orders/order-service.ts
+++ src/modules/orders/order-service.ts
@@ -9,7 +9,7 @@ import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const connectionString = `${process.env.DATABASE_URL}`;
-const pool = new Pool({ connectionString });
+const pool = new Pool({ connectionString, ssl: { rejectUnauthorized: false } });
 const adapter = new PrismaPg(pool);
 const { PrismaClient } = pkg;
 const prisma = new PrismaClient({ adapter });
